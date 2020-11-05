57e8213d4a42f294b7b18efcc2898c2a0f321396
BOLIVAR RICARDO RAMIREZ LAGOS <bolivar.ramirez@pucese.edu.ec>
2020-11-05 13:06:44 -0500

    Fin de clase

---
+++ ConsolePruebaGit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePruebaGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo C#");
+            Console.WriteLine("Integracion de VS con Git");
+            Console.ReadKey();
+
+        }
+    }
+}
